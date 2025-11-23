fa39e43af510dccfc6fb7857365783eb06357ecc
praveen033 <praveenkumar7572@gmail.com>
2025-11-23 15:36:40 +0530

    demo

--- JS_Foundation/part9_adv/asyncAwaitpart2.js
+++ JS_Foundation/part9_adv/asyncAwaitpart2.js
@@ -24,6 +24,8 @@ async function getBlogData() {
       fetchCommentData(),
     ]);
 
+    
+
     console.log(postData);
     console.log(commentData);
 
@@ -32,4 +34,74 @@ async function getBlogData() {
     console.error("Error fetching blog data", error);
   }
 }
-getBlogData();
+// getBlogData();
+
+
+
+function simulateAsyncTask() {
+    return new Promise((resolve)=>{
+        
+       setTimeout(() => {
+           resolve ( `Task finished`);
+       }, 2000); 
+    });
+}
+
+async function getAsyncTask(){
+    try {
+        console.log (`Task started`);
+        let value = await simulateAsyncTask();
+        // console.log (`Task finished`);
+        console.log (value);
+    } catch (e) {
+        console.log(error);
+    }
+}
+
+ getAsyncTask();
+
+
+
+
+function Task1() {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+           resolve ( `Task 1 finished`);
+       }, 1000); 
+    });
+}
+function Task2() {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+           resolve ( `Task 2 finished`);
+       }, 2000); 
+    });
+}
+function Task3() {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+           resolve ( `Task 3 finished`);
+       }, 3000); 
+    });
+}
+
+async function simulateMultipleTasks() {
+    try {
+        // const t1 = await Task1();
+        // console.log(t1);
+        // const t2 = await Task2();
+        // console.log(t2);
+        // const t3 = await Task3();
+        // console.log(t3);
+
+        const [t1,t2,t3] = await Promise.all([Task1(), Task2(), Task3()]);
+        console.log(t1);
+        console.log(t2);
+        console.log(t3);
+    } 
+    catch (error) {
+        console.log(error);
+    }
+    
+}
+simulateMultipleTasks();
